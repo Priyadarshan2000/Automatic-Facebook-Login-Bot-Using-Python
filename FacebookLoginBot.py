51aa09f4f2cb021f1ad198d4de254eac3c82dd65
Priyadarshan2000 <62868878+Priyadarshan2000@users.noreply.github.com>
2020-09-29 23:00:46 +0530

    Add files via upload

---
+++ FacebookLoginBot.py
@@ -0,0 +1,16 @@
+# Priyadarshan Ghosh
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+browser=webdriver.Chrome('C:\\webdrivers\\chromedriver.exe')
+
+browser.get("https://www.facebook.com")
+user_id="Your Facebook id" # Here enter your facebook Id
+password="your Facebook Password" # Here enter your facebook password
+
+
+element=browser.find_element_by_id("email")
+element.send_keys(user_id)
+
+passwor=browser.find_element_by_id("pass")
+passwor.send_keys(password)
